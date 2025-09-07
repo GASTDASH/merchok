01ea46d361dcc4c3610816f21016caf236d95d43
GASTDASH <gastdash@gmail.com>
2025-09-07 22:33:41 +0300

    Добавлены новые иконки

--- lib/core/constants/icon_names.dart
+++ lib/core/constants/icon_names.dart
@@ -1,16 +1,27 @@
 abstract class IconNames {
-  static const house = 'assets/icons/House_01.svg';
-  static const archive = 'assets/icons/Archive.svg';
-  static const chartBar = 'assets/icons/Chart_Bar_Vertical_01.svg';
-  static const settings = 'assets/icons/Settings.svg';
   static const add = 'assets/icons/Add_Plus.svg';
-  static const shoppingBag = 'assets/icons/Shopping_Bag_02.svg';
+  static const archive = 'assets/icons/Archive.svg';
+  static const book = 'assets/icons/Book.svg';
   static const calendar = 'assets/icons/Calendar_Event.svg';
-  static const search = 'assets/icons/Search_Magnifying_Glass.svg';
-  static const tag = 'assets/icons/Tag.svg';
+  static const chartBar = 'assets/icons/Chart_Bar_Vertical_01.svg';
+  static const chartLine = 'assets/icons/Chart_Line.svg';
+  static const down = 'assets/icons/Chevron_Down.svg';
+  static const discount = 'assets/icons/Discount.svg';
+  static const dollar = 'assets/icons/Dollar.svg';
   static const edit = 'assets/icons/Edit_Pencil_01.svg';
-  static const shoppingCart = 'assets/icons/Shopping_Cart_02.svg';
-  static const remove = 'assets/icons/Remove_Minus.svg';
   static const import = 'assets/icons/File_Upload.svg';
+  static const graph = 'assets/icons/Graph.svg';
+  static const house = 'assets/icons/House_01.svg';
+  static const like = 'assets/icons/Like.svg';
+  static const presentation = 'assets/icons/Presentation.svg';
+  static const remove = 'assets/icons/Remove_Minus.svg';
+  static const search = 'assets/icons/Search_Magnifying_Glass.svg';
+  static const settings = 'assets/icons/Settings.svg';
+  static const shoppingBag = 'assets/icons/Shopping_Bag_02.svg';
+  static const shoppingCart = 'assets/icons/Shopping_Cart_02.svg';
+  static const slider = 'assets/icons/Slider_03.svg';
+  static const star = 'assets/icons/Star.svg';
+  static const tag = 'assets/icons/Tag.svg';
   static const delete = 'assets/icons/Trash_Full.svg';
+  static const user = 'assets/icons/User_Circle.svg';
 }
